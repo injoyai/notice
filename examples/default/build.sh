e12cf1d4b29b5ca059b1bfa3d8533cc60b8203c8
injoyai <1113655791@qq.com>
2025-01-16 10:54:45 +0800

    优化build脚本

--- examples/default/build.sh
+++ examples/default/build.sh
@@ -1,10 +1,9 @@
-name="notice"
-name_linux_arm="notice_linux_arm"
-name_linux_amd64="notice_linux_amd64"
 
 # 编译
+
+name="notice"
 GOOS=windows GOARCH=amd64 go build -v -ldflags="-s -w -extldflags -static" -o ./$name.exe
-echo "windows编译完成..."
+echo "$name 编译完成..."
 echo "开始压缩..."
 upx -9 -k "./$name.exe"
 if [ -f "./$name.ex~" ]; then
@@ -15,32 +14,42 @@ if [ -f "./$name.000" ]; then
 fi
 echo "压缩完成..."
 
-GOOS=linux GOARCH=arm GOARM=7 go build -v -ldflags="-w -s" -o ./$name_linux_arm
-echo "arm7编译完成..."
+name="$notice"_linux_arm
+GOOS=linux GOARCH=arm GOARM=7 go build -v -ldflags="-w -s" -o ./$name
+echo "$name 编译完成..."
 echo "开始压缩..."
-upx -9 -k "./$name_linux_arm"
-if [ -f "./$name_linux_arm.~" ]; then
-  rm "./$name_linux_arm.~"
+upx -9 -k "./$name"
+if [ -f "./$name.~" ]; then
+  rm "./$name.~"
 fi
-if [ -f "./$name_linux_arm.000" ]; then
-  rm "./$name_linux_arm.000"
+if [ -f "./$name.000" ]; then
+  rm "./$name.000"
 fi
 
-GOOS=linux GOARCH=amd64 go build -v -ldflags="-w -s" -o ./$name_linux_amd64
-echo "amd64编译完成..."
+name="$notice"_linux_arm64
+GOOS=linux GOARCH=arm64 go build -v -ldflags="-w -s" -o ./$name
+echo "$name 编译完成..."
 echo "开始压缩..."
-upx -9 -k "./$name_linux_amd64"
-if [ -f "./$name_linux_amd64.~" ]; then
-  rm "./$name_linux_amd64.~"
+upx -9 -k "./$name"
+if [ -f "./$name.~" ]; then
+  rm "./$name.~"
 fi
-if [ -f "./$name_linux_amd64.000" ]; then
-  rm "./$name_linux_amd64.000"
+if [ -f "./$name.000" ]; then
+  rm "./$name.000"
 fi
 
+name="$notice"_linux_amd64
+GOOS=linux GOARCH=amd64 go build -v -ldflags="-w -s" -o ./$name
+echo "$name 编译完成..."
+echo "开始压缩..."
+upx -9 -k "./$name"
+if [ -f "./$name.~" ]; then
+  rm "./$name.~"
+fi
+if [ -f "./$name.000" ]; then
+  rm "./$name.000"
+fi
 
-# 上传到minio
-echo "上传到minio..."
-CMD.exe /C "in upload minio ./$name.exe"
 
 echo "稍后退出.."
 sleep 8
